446aef25752f6cc0f9bab71dc64eb25551939b64
Max <108740779+coder1max@users.noreply.github.com>
2023-10-18 19:18:48 +0200

    Update main.js

--- main.js
+++ main.js
@@ -11,3 +11,17 @@ const observer = new IntersectionObserver((entries) => {
   
 const sectionsCachees = document.querySelectorAll('.caché');
 sectionsCachees.forEach((el) => observer.observe(el))
+
+const secondObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show')
+    }
+  });
+});
+
+const partiesCachees = document.querySelectorAll('.hidden');
+partiesCachees.forEach((el) => secondObserver.observe(el))
